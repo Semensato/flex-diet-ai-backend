b85df3ee3a425545226d39b7aab47b3970c96fd0
= <guilherme_semensato@hotmail.com.br>
2024-01-04 23:00:16 -0300

    feat(add): IUserData specific repository

---
+++ FlexDietAiDAL/Interfaces/IUserData.cs
@@ -0,0 +1,16 @@
+﻿using FlexDietAiDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexDietAiDAL.Interfaces
+{
+    public interface IUserData
+    {
+        public Task<UserData> CreateAsync(UserData _object);
+        public Task<UserData> DeleteAsync(UserData _object);
+        public Task<UserData> UpdateAsync(int Id, UserData _object);
+    }
+}
