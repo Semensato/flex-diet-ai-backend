92ab08d3158ad1c45b9f84c15676d812d5512e02
= <guilherme_semensato@hotmail.com.br>
2024-01-05 17:31:23 -0300

    feat: IUserHealth repository interface

---
+++ FlexDietAiDAL/Interfaces/IUserHealth.cs
@@ -0,0 +1,17 @@
+﻿using FlexDietAiDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexDietAiDAL.Interfaces
+{
+    public interface IUserHealth
+    {
+        public Task<UserHealth> CreateAsync(UserHealth _object);
+        public Task<UserHealth> DeleteAsync(UserHealth _object);
+        public Task<UserHealth> UpdateAsync(int Id, UserHealth _object);
+        public Task<UserHealth?> GetByIdAsync(int Id);
+    }
+}
